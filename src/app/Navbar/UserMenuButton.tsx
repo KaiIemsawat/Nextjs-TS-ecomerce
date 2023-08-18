321dc9e6681723b8832a833b43fde0861430806f
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-08-18 09:21:01 -0400

    login with google account feature

---
+++ src/app/Navbar/UserMenuButton.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { Session } from "next-auth";
+import Image from "next/image";
+import profilePicPlaceholder from "@/assets/profile-pic-placeholder.png";
+import { signIn, signOut } from "next-auth/react";
+
+interface UserMenuBtnProps {
+  session: Session | null;
+}
+
+export default function UserMenuBtn({ session }: UserMenuBtnProps) {
+  const user = session?.user;
+  return (
+    <div className="dropdown dropdown-end">
+      <label tabIndex={0} className="btn btn-circle btn-ghost">
+        {user ? (
+          <Image
+            src={user?.image || profilePicPlaceholder}
+            alt="Profile picture"
+            width={40}
+            height={40}
+            className="w-10 rounded-full"
+          />
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="h-6 w-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75"
+            />
+          </svg>
+        )}
+      </label>
+      <ul
+        tabIndex={0}
+        className="menu dropdown-content rounded-box menu-sm z-30 mt-3 w-52 bg-base-100 p-2 shadow"
+      >
+        <li>
+          {user ? (
+            <button onClick={() => signOut({ callbackUrl: "/" })}>
+              Sign out
+            </button>
+          ) : (
+            <button onClick={() => signIn()}>Sign in</button>
+          )}
+        </li>
+      </ul>
+    </div>
+  );
+}
